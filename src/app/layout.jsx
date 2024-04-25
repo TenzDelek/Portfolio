97d2d955a8bc092b6f4e911b2038dd636f33a0e7
TenzDelek <tibetdelek@gmail.com>
2024-04-25 23:49:17 +0530

    design cover

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
--- src/components/Card/ProjectCard.jsx
+++ src/components/Card/ProjectCard.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-
-const ProjectCard = ({date,type,title}) => {
+import Image from "next/image";
+import React from "react";
+const ProjectCard = ({ date, type, title, imgs }) => {
+ 
   return (
-    <div className=' p-2 w-full md:hover:bg-[#1A1A1A] hover:bg-[#292929] transition'>
-        <div className={`relative hidden md:flex  bg-[#606060] h-44 rounded-sm w-56 `}>
-            {/* for image */}
-        </div>
-        <div className=' flex justify-between items-center'>
-        <p className=' text-sm font-semibold mt-2'>{title}</p> 
-        <p className=' text-xs font-medium text-[#858585] mt-2'> {date}</p>
-        </div>
-       
-        <p className=' text-xs font-medium text-[#858585]'>{type}</p>
-        </div>
-  )
-}
+    <div className=" p-2 w-full md:hover:bg-[#1A1A1A] hover:bg-[#292929] transition">
+      <div
+        className={`relative hidden md:flex  bg-[#292929] h-44 rounded-sm w-56 `}
+      >
+        {/* for image */}
+        <Image src={imgs} fill />
+      </div>
+      <div className=" flex justify-between items-center">
+        <p className=" text-sm font-semibold mt-2">{title}</p>
+        <p className=" text-xs font-medium text-[#858585] mt-2"> {date}</p>
+      </div>
 
-export default ProjectCard
+      <p className=" text-xs font-medium text-[#858585]">{type}</p>
+    </div>
+  );
+};
+
+export default ProjectCard;

6d16e1d6dc5acf09bd8b57675ea07a619c18f29b
TenzDelek <tibetdelek@gmail.com>
2024-06-02 16:01:51 +0530

    metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Tenzin Delek",
   description: "Portfolio of Tenzin Delek for development and design",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({ children }) {

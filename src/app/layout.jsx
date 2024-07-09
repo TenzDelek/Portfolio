e59807f5f5e1ac6b35df12aaa3803eca326aef2c
TenzDelek <tibetdelek@gmail.com>
2024-07-09 22:52:52 +0530

    LAYOUT CHANGES FOR OG

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,11 +8,27 @@ export const metadata = {
   title: "Tenzin Delek",
   description: "Portfolio of Tenzin Delek for development and design",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  openGraph: {
+    title: 'Tenzin Delek Portfolio',
+    description: 'Portfolio of Tenzin Delek for development and design',
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'PORTFOLIO 2024',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta property="og:image" content="<generated>" />
+<meta property="og:image:type" content="<generated>" />
+<meta property="og:image:width" content="<generated>" />
+<meta property="og:image:height" content="<generated>" />
       <body className={inter.className}>
         <div className="lg:w-[720px] md:w-[520px] w-[380px] m-auto  px-2  flex flex-col justify-between">
         <Navbar/>
